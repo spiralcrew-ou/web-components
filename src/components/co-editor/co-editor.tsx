f5dbd707fd84ff4df38c91eb1d19b5957f58c3c5
Leonardo Gabriel Leenen <leonardo.leenen@agtech.com.ar>
2019-04-23 09:38:09 -0300

    Adding support left menu

--- src/components/co-editor/co-editor.tsx
+++ src/components/co-editor/co-editor.tsx
@@ -1,10 +1,9 @@
 import { Component, Prop, State, Listen } from '@stencil/core';
 import { MDCMenu } from '@material/menu';
-import  {uuidv4} from '../../utils/utils';
+import { uuidv4 } from '../../utils/utils';
 
 let menu = null
-let editor = null
-
+let toolbar = null
 
 
 @Component({
@@ -35,90 +34,92 @@ export class COEditor {
     menu.open = !menu.open
   }
 
+  handleOpenToolbar = (e) => {
+    console.log(e)
+    toolbar.hoistMenuToBody()
+    //const el = document.querySelector('.editorToolbar')
+    // const _x = window.innerWidth - el.getBoundingClientRect().width - 40
+    // const _y = window.innerHeight - el.getBoundingClientRect().height - 90
+    toolbar.setAbsolutePosition(e.x, e.y)
+    toolbar.open = !toolbar.open
+  }
+
   save = () => {
-    editor.save().then(outputdata => {
-      console.log(outputdata)
-    })
+    
   }
 
   componentWillLoad = () => {
     this.blocks = [{
-      id: '0',
-      type: 'co-title1',
-      content: 'este es el titulo'
-    }, {
-      id: '01',
-      type: 'co-title2',
-      content: 'este es el titulo2'
-    }, {
-      id: '012',
-      type: 'co-subtitle1',
-      content: 'Subtitulo 1'
-    }, {
-      id: '013',
-      type: 'co-subtitle2',
-      content: 'Subtitulo 2'
-    }
-      , {
-      id: '1',
+      id: uuidv4(),
       type: 'co-paragraph',
-      content: 'Este es un contenidoww'
-    },
-    {
-      id: '2',
-      type: 'co-paragraph',
-      content: 'Este es otro'
-    }
-    ]
+      content: ''
+    }]
   }
 
   @Listen('keydown')
-    handleKeyDown(ev: KeyboardEvent){
-        const newLine = {
-          id: uuidv4(),
-          type: 'co-paragraph',
-          content: ''
-        }
-
-        if (ev.key === 'Enter') {
-          const dummy = Object.assign([],this.blocks)
-          const index  =  this.blocks.findIndex( e => e.id === this.blockActiveId )
-          dummy[index].content.replace(/\n/g,'')
-          dummy.splice(index + 1, 0, newLine )
-          this.blocks = dummy
-          ev.preventDefault()
-        }
-
-        if ((ev.key === 'Backspace')  && (document.getElementById('CO-ELID-' + this.blockActiveId).innerHTML.length<=1)){
-          const dummy = Object.assign([],this.blocks)
-          this.blocks = dummy.filter( e => e.id != this.blockActiveId)      
-        }
-          
+  handleKeyDown(ev: KeyboardEvent) {
+    const newLine = {
+      id: uuidv4(),
+      type: 'co-paragraph',
+      content: ''
     }
 
+    if (ev.key === 'Enter') {
+      const dummy = Object.assign([], this.blocks)
+      const index = this.blocks.findIndex(e => e.id === this.blockActiveId)
+      dummy[index].content.replace(/\n/g, '')
+      dummy.splice(index + 1, 0, newLine)
+      this.blocks = dummy
+      ev.preventDefault()
+    }
+
+    if ((ev.key === 'Backspace') && (document.getElementById('CO-ELID-' + this.blockActiveId).innerHTML.length <= 1)) {
+      const dummy = Object.assign([], this.blocks)
+      this.blocks = dummy.filter(e => e.id != this.blockActiveId)
+    }
+
+  }
+
   @Listen('activeBlock')
-  handleBlockActive(ev: CustomEvent){
-    
+  handleBlockActive(ev: CustomEvent) {
+
     this.blockActiveId = ev.detail
   }
 
   componentDidLoad = () => {
     menu = new MDCMenu(document.querySelector('.mdc-menu'))
+    toolbar = new MDCMenu(document.querySelector('.editorToolbar'))
   }
 
-  
-  renderMenuBlock = block => {
-    console.log(block)
+
+  renderToolbar = () => {
+    
+    return <div class="editorToolbar mdc-menu mdc-menu-surface ">
+      <ul class="mdc-list mdc-typography--body1" role="menu" aria-hidden="true" aria-orientation="vertical" tabindex="-1" >
+        <li class="mdc-list-item" role="menuitem">
+          <i class="material-icons mdc-list-item__graphic" aria-hidden="true">format_size</i>
+          <span class="mdc-list-item__text">Header1</span>
+        </li>
+        <li class="mdc-list-item" role="menuitem">
+          <i class="material-icons mdc-list-item__graphic" aria-hidden="true">format_size</i>
+          <span class="mdc-list-item__text">Header2</span>
+        </li>
+        <li class="mdc-list-item" role="menuitem">
+          <i class="material-icons mdc-list-item__graphic" aria-hidden="true">format_textdirection_l_to_r</i>
+          <span class="mdc-list-item__text">Paragraph</span>
+        </li>
+      </ul>
+    </div>
   }
 
   renderBlock = block => {
     switch (block.type) {
       case 'co-title1':
-        return <co-title1 block_id={block.id}  content={block.content}></co-title1>
+        return <co-title1 block_id={block.id} content={block.content}></co-title1>
       case 'co-title2':
         return <co-title2 block_id={block.id} content={block.content}></co-title2>
       case 'co-subtitle1':
-        return <co-subtitle1  block_id={block.id}content={block.content}></co-subtitle1>
+        return <co-subtitle1 block_id={block.id} content={block.content}></co-subtitle1>
       case 'co-subtitle2':
         return <co-subtitle2 block_id={block.id} content={block.content}></co-subtitle2>
       case 'co-paragraph':
@@ -135,11 +136,13 @@ export class COEditor {
       <div>
         {this.blocks.map(block => (
           <div class='block'>
-            <button class="mdc-icon-button material-icons ghost">menu</button>
+            <button class="mdc-icon-button material-icons ghost" onClick={ e => this.handleOpenToolbar(e)}>menu</button>
             {this.renderBlock(block)}
             <button class="mdc-icon-button material-icons ghost">more_vert</button>
           </div>
         ))}
+
+        {this.renderToolbar()}
         <button id="menu-button" class="mdc-fab app-fab--absolute" aria-label="Favorite" onClick={this.handleOpen}>
           <span class="mdc-fab__icon material-icons" >create</span>
         </button>
