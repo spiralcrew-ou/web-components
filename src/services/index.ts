4578fa67791ccbca1d40ada0ccd8307271018926
guillemcordoba <guillem.cordoba@gmail.com>
2019-06-03 18:45:28 +0200

    Point to holochain

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,32 +1,34 @@
 import { UprtclMultiplatform } from './multiplatform/uprtcl.multiplatform';
-import { UprtclLocal } from './local/uprtcl.local';
 import { DataMultiplatform } from './multiplatform/data.multiplatform';
-import { DataLocal } from './local/data.local';
-import { DraftLocal } from './local/draft.local';
 
-//import { DataHolochain } from './holochain/data.holochain';
-//import { UprtclHolochain } from './holochain/uprtcl.holochain';
-//import { DiscoveryHolochain } from './holochain/discovery.holochain';
+//import { UprtclLocal } from './local/uprtcl.local';
+//import { DataLocal } from './local/data.local';
+//import { DraftLocal } from './local/draft.local';
+
+import { DraftHolochain } from './holochain/draft.holochain';
+import { DataHolochain } from './holochain/data.holochain';
+import { UprtclHolochain } from './holochain/uprtcl.holochain';
+import { DiscoveryHolochain } from './holochain/discovery.holochain';
 
 export const uprtclMultiplatform = new UprtclMultiplatform(
   {
-    local: { service: new UprtclLocal(), discovery: null }
-    /* holochain: {
+    // local: { service: new UprtclLocal(), discovery: null }
+    'holochain://QmVE6tmbcHPDkC2ULueG73hH2RcqNmrcDLY4RcuuwWQoN7': {
       service: new UprtclHolochain(),
       discovery: new DiscoveryHolochain()
-    } */
+    }
   },
-  'local'
+  'holochain://QmVE6tmbcHPDkC2ULueG73hH2RcqNmrcDLY4RcuuwWQoN7'
 );
 
 export const dataMultiplatform = new DataMultiplatform(
   {
-    local: { service: new DataLocal(), discovery: null }
-    /* holochain: {
-    service: new DataHolochain(),
-    discovery: new DiscoveryHolochain()
-  } */
+    //local: { service: new DataLocal(), discovery: null }
+    'holochain://QmVE6tmbcHPDkC2ULueG73hH2RcqNmrcDLY4RcuuwWQoN7': {
+      service: new DataHolochain(),
+      discovery: new DiscoveryHolochain()
+    }
   },
-  new DraftLocal(),
-  'local'
+  new DraftHolochain(),
+  'holochain://QmVE6tmbcHPDkC2ULueG73hH2RcqNmrcDLY4RcuuwWQoN7'
 );
