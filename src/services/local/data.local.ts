812d349b25e8148871784bea1896b1cf07b33408
Leonardo Gabriel Leenen <leonardo.leenen@agtech.com.ar>
2019-06-07 07:06:56 -0300

    Adding Tailwind support

--- src/services/local/data.local.ts
+++ src/services/local/data.local.ts
@@ -2,7 +2,7 @@ import { DataService } from '../data.service';
 import { TextNode } from '../../types';
 import {insertTextNode,getTextNode} from '../local/dataservices';
 
-
+//SHA2 256 para generar los ID
 export class DataLocal implements DataService {
   createData(data: TextNode): Promise<string> {
     return insertTextNode(data)
