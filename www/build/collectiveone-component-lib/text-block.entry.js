69a0456b61c07d7b6d42b6dcb7d2b28c02698f64
Pepo <pepo.ospina@gmail.com>
2019-06-27 20:28:35 +0200

    added www build to repo

---
+++ www/build/collectiveone-component-lib/text-block.entry.js
@@ -0,0 +1,66 @@
+const h = window.CollectiveoneComponentLib.h;
+
+class TextBlock {
+    constructor() {
+        // Keep timeout to cancel debounce typing
+        this.changeTextTimeout = null;
+    }
+    componentDidLoad() {
+        const element = this.element.shadowRoot.getElementById(this.id);
+        if (element)
+            element.focus();
+    }
+    componentWillUpdate() {
+        const element = this.element.shadowRoot.getElementById(this.id);
+        if (element)
+            element.innerHTML = this.text;
+    }
+    render() {
+        return (h("div", null,
+            h("span", { id: this.id, class: 'text-block', "data-placeholder": 'Start typing...', contenteditable: 'true' }, this.text)));
+    }
+    onKeyDown(event) {
+        if (event.key !== 'Enter') {
+            event.stopPropagation();
+            // Debounce to not save every key down but every two seconds of change
+            if (this.changeTextTimeout) {
+                clearTimeout(this.changeTextTimeout);
+            }
+            this.changeTextTimeout = setTimeout(() => {
+                // Get the new text and emit it
+                const newText = this.element.shadowRoot.getElementById(this.id)
+                    .innerText;
+                this.contentChanged.emit(newText);
+            }, 2000);
+        }
+    }
+    static get is() { return "text-block"; }
+    static get encapsulation() { return "shadow"; }
+    static get properties() { return {
+        "element": {
+            "elementRef": true
+        },
+        "id": {
+            "type": String,
+            "attr": "id"
+        },
+        "text": {
+            "type": String,
+            "attr": "text"
+        }
+    }; }
+    static get events() { return [{
+            "name": "content-changed",
+            "method": "contentChanged",
+            "bubbles": true,
+            "cancelable": true,
+            "composed": true
+        }]; }
+    static get listeners() { return [{
+            "name": "keydown",
+            "method": "onKeyDown"
+        }]; }
+    static get style() { return "[contenteditable=true]:empty:before {\n  content: attr(data-placeholder);\n  opacity: 0.7; }\n\n[contenteditable] {\n  display: block;\n  /* For Firefox */\n  font-family: \"Open Sans\", sans-serif;\n  font-size: 22px; }\n\n[contenteditable=true]:focus {\n  outline: 0px solid transparent; }"; }
+}
+
+export { TextBlock };
---
+++ www/build/collectiveone-component-lib/uprtcl-toolbar.entry.js
@@ -0,0 +1,87 @@
+const h = window.CollectiveoneComponentLib.h;
+
+import { u as uprtclMultiplatform, v as uprtclData } from './chunk-b6e62c79.js';
+import './chunk-84ac4f31.js';
+
+class UptrclToolbar {
+    constructor() {
+        this.creatingPerspective = false;
+        this.contextPerspectives = [];
+        this.uprtcl = uprtclMultiplatform;
+    }
+    async componentWillLoad() {
+        this.contextPerspectives = await this.uprtcl.getContextPerspectives(this.perspective.contextId);
+    }
+    async logUprtcl() {
+        const perspectiveFull = await uprtclData.getPerspectiveFull(this.perspective.id, 5);
+        console.log(perspectiveFull);
+    }
+    componentDidLoad() {
+    }
+    render() {
+        return (h("div", { class: "flex-column" },
+            h("div", { class: "flex-row" },
+                h("select", { disabled: this.contextPerspectives.length === 0, onChange: (e) => {
+                        this.selectPerspective.emit(e.target.selectedOptions[0].value);
+                    } }, this.contextPerspectives.length === 0 ? (h("option", null, "Loading...")) : (this.contextPerspectives.map(perspective => (h("option", { value: perspective.id }, perspective.name))))),
+                h("button", { onClick: () => (this.creatingPerspective = true) }, "New Perspective"),
+                h("button", { onClick: () => this.createCommit.emit() }, "Commit"),
+                h("button", { onClick: () => this.logUprtcl() }, "Log")),
+            this.creatingPerspective ? (h("div", { class: "flex-row" },
+                h("select", { id: "new-perspective-provider" }, this.uprtcl.getServiceProviders().map(service => (h("option", { value: service }, service)))),
+                h("input", { id: "new-perspective-name", type: "text" }),
+                h("button", { onClick: () => this.createPerspective() }, "Create"))) : ('')));
+    }
+    createPerspective() {
+        const name = this.element.shadowRoot.getElementById('new-perspective-name');
+        const provider = this.element.shadowRoot.getElementById('new-perspective-provider');
+        this.createPerspectiveEvent.emit({
+            name: name['value'],
+            serviceProvider: provider.selectedOptions[0].value
+        });
+        this.creatingPerspective = false;
+    }
+    static get is() { return "uprtcl-toolbar"; }
+    static get encapsulation() { return "shadow"; }
+    static get properties() { return {
+        "contextPerspectives": {
+            "state": true
+        },
+        "creatingPerspective": {
+            "state": true
+        },
+        "defaultService": {
+            "type": String,
+            "attr": "default-service"
+        },
+        "element": {
+            "elementRef": true
+        },
+        "perspective": {
+            "type": "Any",
+            "attr": "perspective"
+        }
+    }; }
+    static get events() { return [{
+            "name": "createCommit",
+            "method": "createCommit",
+            "bubbles": true,
+            "cancelable": true,
+            "composed": true
+        }, {
+            "name": "selectPerspective",
+            "method": "selectPerspective",
+            "bubbles": true,
+            "cancelable": true,
+            "composed": true
+        }, {
+            "name": "createPerspective",
+            "method": "createPerspectiveEvent",
+            "bubbles": true,
+            "cancelable": true,
+            "composed": true
+        }]; }
+    static get style() { return ".flex-column {\n  display: -ms-flexbox;\n  display: flex;\n  -ms-flex-direction: column;\n  flex-direction: column; }\n\n.flex-row {\n  display: -ms-flexbox;\n  display: flex;\n  -ms-flex-direction: row;\n  flex-direction: row; }"; }
+}
+
+export { UptrclToolbar as UprtclToolbar };
