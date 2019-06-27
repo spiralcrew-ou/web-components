8e883b6906e983fa2d485ade78c1693ec3b95e4f
Guillem Cordoba <guillem.cordoba@gmail.com>
2019-06-27 16:12:42 +0200

    Background tasks pending indicator

--- src/actions.ts
+++ src/actions.ts
@@ -1,19 +1,18 @@
-import { uprtclData } from "./services/uprtcl-data";
-import { PerspectiveFull, TextNodeFull } from "./types";
+import { uprtclData } from './services/uprtcl-data';
+import { PerspectiveFull, TextNodeFull } from './types';
 
 export enum NodeType {
-  title = "title",
-  paragraph = "paragraph"
+  title = 'title',
+  paragraph = 'paragraph'
 }
 export interface Block {
-  id: string,
-  children: string[],
-  status: string,  
-  content: string,
-  style: NodeType,
-  serviceProvider: string
+  id: string;
+  children: string[];
+  status: string;
+  content: string;
+  style: NodeType;
+  serviceProvider: string;
 }
-
 
 /** ----- SUPPORT FUNCTIONS ------ */
 
@@ -24,14 +23,12 @@ const getPerspectiveData = (perspective: PerspectiveFull): TextNodeFull => {
     if (perspective.head) {
       return perspective.head.data;
     } else {
-      null
+      null;
     }
   }
 };
 
-const mapPerspectiveToBlock = (
-  perspectiveFull: PerspectiveFull
-): Block => {
+const mapPerspectiveToBlock = (perspectiveFull: PerspectiveFull): Block => {
   let data = getPerspectiveData(perspectiveFull);
 
   const block: Block = {
@@ -46,47 +43,50 @@ const mapPerspectiveToBlock = (
   return block;
 };
 
-const setTreeWithPerspectiveRec = (
-  perspectiveFull: PerspectiveFull,
-  tree) => {
-  
+const setTreeWithPerspectiveRec = (perspectiveFull: PerspectiveFull, tree) => {
   let block = mapPerspectiveToBlock(perspectiveFull);
   let data = getPerspectiveData(perspectiveFull);
-  
+
   if (data) {
     data.links.map(link => {
       setTreeWithPerspectiveRec(link.link, tree);
       block.children.push(link.link.id);
     });
   }
-  
+
   tree[perspectiveFull.id] = block;
-}
+};
 
 const reloadMasterTree = async (getState): Promise<any> => {
   let perspectiveFull = await uprtclData.getPerspectiveFull(
-    getState().workpad.rootId, -1);
+    getState().workpad.rootId,
+    -1
+  );
 
-  let _tree = {}
+  let _tree = {};
   setTreeWithPerspectiveRec(perspectiveFull, _tree);
 
   let textNodeTree = await uprtclData.toTextNodeTree(getState().workpad.rootId);
-  console.log('[REDUX] Reload master tree.', {textNodeTree, perspectiveFull, _tree });
+  console.log('[REDUX] Reload master tree.', {
+    textNodeTree,
+    perspectiveFull,
+    _tree
+  });
   return _tree;
-}
+};
 
 /** ----- ACTIONS FUNCTIONS ------ */
 
-/** This method sets a new root ID and deletes the 
+/** This method sets a new root ID and deletes the
  * tree. It should be followed by a reload tree.
  */
-export const initTree = (_rootId) => {
-  return (dispatch) => {
+export const initTree = _rootId => {
+  return dispatch => {
     dispatch({
-      type: "INIT_TREE",
+      type: 'INIT_TREE',
       rootId: _rootId,
       tree: {}
-    })
+    });
   };
 };
 
@@ -94,10 +94,10 @@ export const reloadTree = () => {
   return async (dispatch, getState) => {
     let _tree = await reloadMasterTree(getState);
     dispatch({
-      type: "RELOAD_TREE",
+      type: 'RELOAD_TREE',
       tree: _tree
     });
-    dispatch(renderingWorkpad(false))
+    dispatch(renderingWorkpad(false));
   };
 };
 
@@ -108,26 +108,29 @@ export const reloadTree = () => {
  */
 export const setContent = (blockId: string, content: string) => {
   return async (dispatch, getState) => {
-
     await uprtclData.setDraftText(blockId, content);
     const perspectiveFull = await uprtclData.getPerspectiveFull(blockId, 0);
 
     let block = getState().workpad.tree[blockId];
     block = mapPerspectiveToBlock(perspectiveFull);
-    const tree = Object.assign({},getState().workpad.tree)
-    tree[blockId] = block
+    const tree = Object.assign({}, getState().workpad.tree);
+    tree[blockId] = block;
     dispatch({ type: 'SET_CONTENT', tree });
   };
 };
 
-export const newBlock = (blockId: string, _content: string, parentId: string, index: number) => {
+export const newBlock = (
+  blockId: string,
+  _content: string,
+  parentId: string,
+  index: number
+) => {
   return async (dispatch, getState) => {
-
     const tree = getState().workpad.tree;
     const initNode: Block = tree[blockId];
 
     switch (initNode.style) {
-      case "title":
+      case 'title':
         /** An enter on a title will create an empty subcontext *
          *  as the first subcontext of the title context.       */
         uprtclData.initContextUnder(
@@ -139,11 +142,14 @@ export const newBlock = (blockId: string, _content: string, parentId: string, in
         );
         break;
 
-      case "paragraph":
+      case 'paragraph':
         /** An enter on a paragraph will create an empty context *
          *  as the next-sibling of that paragraph.               */
         const parent: Block = tree[parentId];
-        if (!parent) throw new Error(`Parent perspective ${parentId} not found in the tree`);
+        if (!parent)
+          throw new Error(
+            `Parent perspective ${parentId} not found in the tree`
+          );
 
         await uprtclData.initContextUnder(
           parent.serviceProvider,
@@ -164,30 +170,35 @@ export const newBlock = (blockId: string, _content: string, parentId: string, in
   };
 };
 
-export const setStyle =  (blockId: string, newStyle: NodeType, parentId: string, index: number) => {
+export const setStyle = (
+  blockId: string,
+  newStyle: NodeType,
+  parentId: string,
+  index: number
+) => {
   return async (dispatch, getState) => {
-    
     const tree = getState().workpad.tree;
     const block: Block = tree[blockId];
     const parent: Block = tree[parentId];
-    if (!parent) throw new Error(`Parent perspective ${parentId} not found in the tree`);
+    if (!parent)
+      throw new Error(`Parent perspective ${parentId} not found in the tree`);
 
     /** set the new style */
     let oldStyle = block.style;
     block.style = newStyle;
     await uprtclData.setDraftType(blockId, newStyle);
 
-    switch(oldStyle) {
-      case NodeType.title: 
+    switch (oldStyle) {
+      case NodeType.title:
         switch (newStyle) {
-          
           /** title to title: setting the same view changes nothing */
-          case NodeType.title: 
+          case NodeType.title:
             return;
-          
-          /** title to paragraph: changing the type of a title to a paragraph 
-           *  will move all its subcontexts as youngr siblings of the new typed 
-           *  paragraph */            
+
+          /** title to paragraph: changing the type of a title to a paragraph
+           *  will move all its subcontexts as youngr siblings of the new typed
+           *  paragraph */
+
           case NodeType.paragraph:
             /** removing in sequence (parallel wont work due to index finding) */
             for (let childIx = 0; childIx < block.children.length; childIx++) {
@@ -201,31 +212,30 @@ export const setStyle =  (blockId: string, newStyle: NodeType, parentId: string,
               await uprtclData.insertPerspective(
                 parentId,
                 childId,
-                  index + childIx + 1);
+                index + childIx + 1
+              );
             }
             break;
         }
-      break;
+        break;
 
       case NodeType.paragraph:
-
         switch (newStyle) {
-
           /** paragraph to paragraph: setting the same view changes nothing */
-          case (NodeType.paragraph):
+          case NodeType.paragraph:
             return;
-          
-          /** paragraph to title: Changing the type of a paragraph to a title 
-           * will move all the younger sibling contexts of the paragraph as 
+
+          /** paragraph to title: Changing the type of a paragraph to a title
+           * will move all the younger sibling contexts of the paragraph as
            * subcontexts of the new title. */
-            case (NodeType.title):
+          case NodeType.title:
             let youngerSyblings = parent.children.splice(index + 1);
 
             /** just move the syblings up to the first one which is a title */
             let indexOfTitleSybling = youngerSyblings.findIndex(syblingId => {
               let sybling: Block = tree[syblingId];
-              return sybling.style === NodeType.title
-            })
+              return sybling.style === NodeType.title;
+            });
             /** there is a title younger sybling */
             if (indexOfTitleSybling !== -1) {
               youngerSyblings = youngerSyblings.slice(0, indexOfTitleSybling);
@@ -236,56 +246,62 @@ export const setStyle =  (blockId: string, newStyle: NodeType, parentId: string,
               /** remove n times the next block */
               await uprtclData.removePerspective(parent.id, index + 1);
             }
-            
+
             /** adding in sequence */
             for (let sybIx = 0; sybIx < youngerSyblings.length; sybIx++) {
               let sybId = youngerSyblings[sybIx];
               await uprtclData.insertPerspective(blockId, sybId, -1);
             }
-          break;
+            break;
         }
-      break;
+        break;
     }
 
     /** force update */
     dispatch({ type: 'SET_STYLE', block });
-    dispatch(reloadTree())
-  }
-}
+    dispatch(reloadTree());
+  };
+};
 
-
-/**   
+/**
  * This method removes a block (perspective indeed) from tree. It removes
  * all its children automatically, se beware!
  */
 export const removeBlock = (parentId: string, index: number) => {
-  return async(dispatch) => {
+  return async dispatch => {
     await uprtclData.removePerspective(parentId, index);
     dispatch(reloadTree());
-  }
+  };
 };
 
-/**   
+/**
  * If the block is of type title, and its parent is not the rootId (meaning
  * the block is at level two or below), it makes the block (together with its children)
  * a younger sybling of the title parent
  */
-export const indentLeft = (blockId: string, parentId: string, index: number) => {
+export const indentLeft = (
+  blockId: string,
+  parentId: string,
+  index: number
+) => {
   return async (dispatch, getState) => {
-
     const tree = getState().workpad.tree;
     const rootId = getState().workpad.rootId;
 
-    if ((parentId === rootId) || (blockId === rootId)) return;
-    
+    if (parentId === rootId || blockId === rootId) return;
+
     /** level 2 or more */
     const block: Block = tree[blockId];
 
     if (block.style !== NodeType.title) return;
 
-    let grandParentId = Object.keys(tree).find(key => tree[key].children.includes(parentId));
-    let parentIx = tree[grandParentId].children.findIndex(id => id === parentId);
-    
+    let grandParentId = Object.keys(tree).find(key =>
+      tree[key].children.includes(parentId)
+    );
+    let parentIx = tree[grandParentId].children.findIndex(
+      id => id === parentId
+    );
+
     if (!grandParentId) return;
     if (parentIx == -1) return;
 
@@ -296,29 +312,28 @@ export const indentLeft = (blockId: string, parentId: string, index: number) => 
     await uprtclData.insertPerspective(grandParentId, blockId, parentIx + 1);
 
     dispatch(reloadTree());
-  }
+  };
 };
 
-
- /** Commits the draft of the block specified by blockId and recursively
+/** Commits the draft of the block specified by blockId and recursively
 f all its children. Send the rootId to commit the entire document.
  */
 export const commitGlobal = (blockId: string, message: string = '') => {
   return async (dispatch, getState) => {
-
-    let block:Block = getState().workpad.tree[blockId]
+    let block: Block = getState().workpad.tree[blockId];
 
     /** create the data in the perspective provider by default */
     let perspective = await uprtclData.uprtcl.getPerspective(blockId);
     let provider = perspective.origin;
-    
+
     await uprtclData.commit(
       provider,
       blockId,
-      message, 
+      message,
       new Date().getTime(),
-      true)
-    
+      true
+    );
+
     dispatch({ type: 'COMMIT_GLOBAL', block });
     dispatch(reloadTree());
   };
@@ -326,142 +341,178 @@ export const commitGlobal = (blockId: string, message: string = '') => {
 
 export const pullPerspective = (blockId: string) => {
   return async (dispatch, getState) => {
-
-    await uprtclData.pull(blockId)
+    await uprtclData.pull(blockId);
 
     const tree = getState().workpad.tree;
     const block: Block = tree[blockId];
-    
+
     dispatch({ type: 'PULL_MADE', block });
     dispatch(reloadTree());
   };
 };
 
-export const mergePerspective = (toPerspective: string, fromPerspectiveId: string) => {
+export const mergePerspective = (
+  toPerspective: string,
+  fromPerspectiveId: string
+) => {
   return async (dispatch, getState) => {
     const tree = getState().workpad.tree;
     const block: Block = tree[toPerspective];
 
-    await uprtclData.merge(toPerspective, [ fromPerspectiveId ]);
-    
+    await uprtclData.merge(toPerspective, [fromPerspectiveId]);
+
     dispatch({ type: 'MERGE_MADE', block });
     dispatch(reloadTree());
   };
 };
 
-
 /**
- * 
+ *
  */
 export const newPerspective = (
-  blockId: string, 
-  name: string, 
-  serviceProvider: string) => {
-
-  return async (dispatch) => {
-
+  blockId: string,
+  name: string,
+  serviceProvider: string
+) => {
+  return async dispatch => {
     let newPerspectiveId = await uprtclData.createGlobalPerspective(
-      serviceProvider, blockId, name
-    )
+      serviceProvider,
+      blockId,
+      name
+    );
 
     // TODO, reuse a tree reload or something
     let perspectiveFull = await uprtclData.getPerspectiveFull(
-      newPerspectiveId, -1);
-  
-    let _tree = {}
+      newPerspectiveId,
+      -1
+    );
+
+    let _tree = {};
     setTreeWithPerspectiveRec(perspectiveFull, _tree);
 
-    dispatch({ 
-      type: "NEW_PERSPECTIVE", 
+    dispatch({
+      type: 'NEW_PERSPECTIVE',
       rootId: newPerspectiveId,
       tree: _tree
-    })
-  }
-}
+    });
+  };
+};
 
 /**
- * 
+ *
  */
-export const checkoutPerspective = (
-  perspectiveId: string) => {
-
-  return async (dispatch) => {
-
+export const checkoutPerspective = (perspectiveId: string) => {
+  return async dispatch => {
     // TODO, reuse a tree reload or something
     let perspectiveFull = await uprtclData.getPerspectiveFull(
-      perspectiveId, -1);
-  
-    let _tree = {}
+      perspectiveId,
+      -1
+    );
+
+    let _tree = {};
     setTreeWithPerspectiveRec(perspectiveFull, _tree);
-    dispatch({ 
-      type: "CHECKOUT_PERSPECTIVE", 
+    dispatch({
+      type: 'CHECKOUT_PERSPECTIVE',
       rootId: perspectiveId,
       tree: _tree
-    })
-    dispatch(renderingWorkpad(false))
-  }
-}
-        
+    });
+    dispatch(renderingWorkpad(false));
+  };
+};
+
 /**
  * This function open the contextual menu
  */
-export const openMenu = (blockId: string, parentId: string, index: number, calledBy: string) => {
-  return (dispatch) => {
+export const openMenu = (
+  blockId: string,
+  parentId: string,
+  index: number,
+  calledBy: string
+) => {
+  return dispatch => {
     dispatch({
-      type: 'OPEN_MENU', 
-      isClose: false, 
+      type: 'OPEN_MENU',
+      isClose: false,
       inBlockData: {
-        blockId, parentId, index, calledBy
+        blockId,
+        parentId,
+        index,
+        calledBy
       }
-    })
-  }
-}
-       
+    });
+  };
+};
+
 /**
  * This function close the contextual menu
  */
 export const closeMenu = () => {
-  return (dispatch) => {
-    dispatch({ type: 'CLOSE_MENU', isClose:true})   
-  }
-}
+  return dispatch => {
+    dispatch({ type: 'CLOSE_MENU', isClose: true });
+  };
+};
 
-export const setPerspectiveToAct = (perspectiveId) => {
+export const setPerspectiveToAct = perspectiveId => {
   return async dispatch => {
-    let perspective: PerspectiveFull = await uprtclData.getPerspectiveFull(perspectiveId, 0);
-    dispatch({type: 'SET_PERSPECTIVE_TO_ACT', perspective})
-  }
-}
+    let perspective: PerspectiveFull = await uprtclData.getPerspectiveFull(
+      perspectiveId,
+      0
+    );
+    dispatch({ type: 'SET_PERSPECTIVE_TO_ACT', perspective });
+  };
+};
 
-export const setPerspectiveToActAndUpdateContextPerspectives = (perspectiveId:string) => {
+export const setPerspectiveToActAndUpdateContextPerspectives = (
+  perspectiveId: string
+) => {
   return async dispatch => {
-    let perspective: PerspectiveFull = await uprtclData.getPerspectiveFull(perspectiveId, 0);
-    let contextPerspectives = await uprtclData.uprtcl.getContextPerspectives(perspective.context.id);
+    let perspective: PerspectiveFull = await uprtclData.getPerspectiveFull(
+      perspectiveId,
+      0
+    );
+    let contextPerspectives = await uprtclData.uprtcl.getContextPerspectives(
+      perspective.context.id
+    );
 
-    dispatch({type: 'UPDATE_CONTEXT_PERSPECTIVES', perspective, contextPerspectives })
-  }
-}
+    dispatch({
+      type: 'UPDATE_CONTEXT_PERSPECTIVES',
+      perspective,
+      contextPerspectives
+    });
+  };
+};
 
 export const setAvailableProviders = (availableProviders: string[]) => {
-  return (dispatch) => {
-    dispatch({ type: 'SET_AVAILABLE_PROVIDERS', availableProviders})
-  }
-}
+  return dispatch => {
+    dispatch({ type: 'SET_AVAILABLE_PROVIDERS', availableProviders });
+  };
+};
 
 export const setSelectedProvider = (selectedProvider: string) => {
-  return (dispatch) => {         
-    dispatch({type: 'SET_SELECTED_PROVIDER', selectedProvider})
-  }
-}
+  return dispatch => {
+    dispatch({ type: 'SET_SELECTED_PROVIDER', selectedProvider });
+  };
+};
 
 export const setEthAccount = (ethAccount: string) => {
-  return (dispatch) => {         
-    dispatch({type: 'SET_ETH_ACCOUNT', ethAccount})
-  }
-}
-       
+  return dispatch => {
+    dispatch({ type: 'SET_ETH_ACCOUNT', ethAccount });
+  };
+};
+
 export const renderingWorkpad = (value: boolean) => {
-  return (dispatch) => {
-      dispatch({type: 'RENDERING_WORKPAD', isRendering:value})
-    }
-}
+  return dispatch => {
+    dispatch({ type: 'RENDERING_WORKPAD', isRendering: value });
+  };
+};
+
+export const watchTasks = () => {
+  return dispatch => {
+    uprtclData.uprtcl.taskQueue.onTasksPending(() =>
+      dispatch({ type: 'TASKS_PENDING', pendingTasks: true })
+    );
+    uprtclData.uprtcl.taskQueue.onTasksFinished(() =>
+      dispatch({ type: 'TASKS_PENDING', pendingTasks: false })
+    );
+  };
+};
--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,10 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import { combineReducers } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux';
+import { combineReducers } from 'redux';
 import thunk from 'redux-thunk'; // Add-on you might want
 import logger from 'redux-logger'; // Add-on you might want
 
-const composeEnhancer = window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] || compose;
+const composeEnhancer =
+  window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] || compose;
 
 const initialState = {
   workpad: {
@@ -12,89 +13,88 @@ const initialState = {
     block: {},
     isRendering: false,
     perspective: null,
-    contextPerspectives: []
+    contextPerspectives: [],
+    pendingTasks: false
   },
   menu: {
-    isClose: true,
+    isClose: true
   },
   support: {
     availableProviders: [],
     selectedProvider: [],
     ethAccount: ''
   }
-}
+};
 
 export const workpadReducer = (state = { ...initialState.workpad }, action) => {
   switch (action.type) {
     case 'INIT_TREE':
-      return { ...state, ...action }
+      return { ...state, ...action };
     case 'RELOAD_TREE':
-      return { ...state, ...action }
+      return { ...state, ...action };
     case 'SET_CONTENT':
-      return { ...state, ...action }
+      return { ...state, ...action };
     case 'NEW_BLOCK':
-      return { ...state, ...action }
+      return { ...state, ...action };
     case 'SET_STYLE':
-      return { ...state, ...action }    
+      return { ...state, ...action };
     case 'REMOVE_BLOCK':
-      return { ...state, ...action }
+      return { ...state, ...action };
     case 'COMMIT_GLOBAL':
-      return { ...state, ...action }
+      return { ...state, ...action };
     case 'NEW_PERSPECTIVE':
-      return {...state,...action}
+      return { ...state, ...action };
     case 'MERGE':
-      return {...state,...action}
+      return { ...state, ...action };
     case 'PULL_MADE':
-        return {...state,...action}
+      return { ...state, ...action };
     case 'RENDERING_WORKPAD':
-      return {...state,...action}
+      return { ...state, ...action };
     case 'SET_PERSPECTIVE_TO_ACT':
-      return {...state,...action}
+      return { ...state, ...action };
     case 'UPDATE_CONTEXT_PERSPECTIVES':
-      return {...state,...action}
+      return { ...state, ...action };
     case 'CHECKOUT_PERSPECTIVE':
-      return {...state,...action}
+      return { ...state, ...action };
     case 'MERGE_MADE':
-        return {...state,...action}
+      return { ...state, ...action };
+    case 'TASKS_PENDING':
+      return { ...state, ...action };
     
     default:
-      return state
+      return state;
   }
-}
+};
 
-export const menuReducer = (state ={...initialState.menu}, action) => {
+export const menuReducer = (state = { ...initialState.menu }, action) => {
   switch (action.type) {
     case 'OPEN_MENU':
-      return {...state,...action}
+      return { ...state, ...action };
     case 'CLOSE_MENU':
-        return {...state,...action}
+      return { ...state, ...action };
     default:
-      return state 
+      return state;
   }
-}
+};
 
-export const supportReducer = (state ={...initialState.support}, action) => {
+export const supportReducer = (state = { ...initialState.support }, action) => {
   switch (action.type) {
     case 'SET_AVAILABLE_PROVIDERS':
-      return {...state,...action}
+      return { ...state, ...action };
     case 'SET_SELECTED_PROVIDER':
-      return {...state,...action}
+      return { ...state, ...action };
     case 'SET_ETH_ACCOUNT':
-      return {...state,...action}
+      return { ...state, ...action };
     default:
-      return state 
+      return state;
   }
-}
-
+};
 
 const reducers = combineReducers({
   workpad: workpadReducer,
   menu: menuReducer,
   support: supportReducer
-})
+});
 
 export const configureStore = () =>
-  createStore(reducers, composeEnhancer(applyMiddleware(thunk, logger)))
-
-
-
+  createStore(reducers, composeEnhancer(applyMiddleware(thunk, logger)));
